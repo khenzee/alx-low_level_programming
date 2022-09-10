f45bfee6ce46cd86792153178a098525820493b3
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-10 14:42:08 -0700

    gj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main: print the string in the put function
+ * main - print the string in the put function
  * Description: Using the main function
- * this prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
