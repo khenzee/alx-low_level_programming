1e3c9d117a3069c6cb42618598f16b008c6dc1a5
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-10 14:24:13 -0700

    jgfv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - print the string in the put function
- * 
+ * return: 0
  * discription: Using the main function
  * this prints "Programming is like building a multilingual puzzle
- * return: 0
  */
 int main(void)
 {
