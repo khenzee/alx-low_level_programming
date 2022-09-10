05d9c98519af57bc2e4dc8a779a7ef030f2d057a
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-10 12:00:05 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-void puts (void){print_string("Programming is like building a multilingual puzzle/n");return (0)}
+return_type puts (void){print_string("Programming is like building a multilingual puzzle/n");return (0)}
