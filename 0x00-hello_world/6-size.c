461580b81dfe1f759f673312516f75de8727b109
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-11 13:10:20 -0700

    sjf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - you should produce the exact same output as in the example
- * Description: Write a C program that prints the size of various
+ * Description: Write a C program "ithat prints the size of various
  * types on the computer it is compiled and run on
  * Return : 0
  */
