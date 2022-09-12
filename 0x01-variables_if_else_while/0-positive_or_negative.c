c11bef12f4c85e86ae012b62bf86f8210134569c
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-12 15:10:54 -0700

    ogur

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		n = "is positive";
+		char n = "is positive";
 	}
-	if (n = 0)
+	if (n == 0)
 	{
-		n = "is zero";
+		char n = "is zero";
 	}	
 	if (n < 0)
 	{
-		n = "is negative";
+		char n = "is negative";
 	}
 	return (0);
 }
