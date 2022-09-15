0d64003a1f8be84ee953df3c19de223f202501d0
laniyan bukola <laniyanolusegun@gmail.com>
2022-09-15 13:35:12 -0700

    dsaf

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - check the code
+ * Return: 1
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
